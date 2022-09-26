138c5a04f3fbdfaa744b2bebe7a043a620b2a601
TwoDD <juanestebancossio21@gmail.com>
2022-09-26 00:06:01 -0500

    add me component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Navbar } from './components/navbar/Navbar';
-
+import { Me } from './components/me/Me';
 function App() {
   return (
     <div>
       <Navbar/>
+      <Me/>
     </div>
   );
 }
---
+++ src/components/me/Me.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Me.css'
+
+export const Me = () => {
+  return (
+    <div className='container-section-me'>
+      <div><label className='text-me'>Hi, my name is</label></div>
+      <div><label className='tittle-me'>Juan Esteban</label></div>
+      <div><label className='tittle-me'>I build things for the web.</label></div>
+      <div><label className='text-me'>I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</label></div>
+      <div><button>check my course</button></div>
+    </div>
+  )
+}
